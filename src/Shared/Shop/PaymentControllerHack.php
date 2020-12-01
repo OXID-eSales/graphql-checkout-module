2a7c89c762c1ef2a7f798e2055cfbe2e5b220765
hkreuter <heike@reuter-consulting.org>
2020-12-01 10:00:17 +0100

    OXDEV-4164 Class mus not be final

--- src/Shared/Shop/PaymentControllerHack.php
+++ src/Shared/Shop/PaymentControllerHack.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Checkout\Shared\Shop;
 
-final class PaymentControllerHack extends PaymentControllerHack_parent
+class PaymentControllerHack extends PaymentControllerHack_parent
 {
     public function validatePaymentForGraphql(string $paymentId)
     {
